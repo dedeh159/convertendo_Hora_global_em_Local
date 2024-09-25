5b01bf9719c1c4923ba42af026e8ef3ba071107b
Andre Fernandes <dedeh159@gmail.com>
2024-09-25 18:34:51 -0300

    convertendo_Hora_global_em_Local

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+        /*imprimindo todos zoneID: for (String s: ZoneId.getAvailableZoneIds()){
+            System.out.println(s);
+        }*/
+        //convertendo hora global em uma hora do sistema
+        LocalDate r1 = LocalDate.ofInstant(d06, ZoneId.systemDefault());
+
+        //imprimindo no horario de portugal
+        LocalDate r2 = LocalDate.ofInstant(d06, ZoneId.of("Portugal"));
+        LocalDateTime r3 = LocalDateTime.ofInstant(d06, ZoneId.systemDefault());
+        LocalDateTime r4 = LocalDateTime.ofInstant(d06, ZoneId.of("Portugal"));
+
+
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
+        System.out.println("r4 = " + r4);
+
+
+    }
+}
